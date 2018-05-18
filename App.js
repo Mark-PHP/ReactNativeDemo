e15516d49b968c2e779922c6135f4769e35762ca
JJ <jj@gmail.com>
2018-05-18 16:38:47 +0800

    新增webview的方式

--- App.js
+++ App.js
@@ -4,55 +4,86 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    WebView,
+    Dimensions
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const deviceHeight = Dimensions
+    .get("window")
+    .height;
+const deviceWidth = Dimensions
+    .get("window")
+    .width;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+var MARGIN_TOP = Platform.OS === "ios"
+    ? 20
+    : 0;
+
+// 请求动态访问的接口地址
+var getUrl = "https://www.baidu.com";
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {url: ''};
+        this.getRequest(getUrl);
+    }
+
+    getRequest(url) {
+        /*网络请求的配置*/
+        var opts = {
+            method: "GET"
+        };
+
+        fetch(url, opts)
+            .then((response) => {
+                return response.text();
+            })
+            .then((responseText) => {
+                this.setState(previous => {
+                    // return {url: responseText};
+                    // 测试的话写死
+                    return {url: getUrl};
+                });
+            })
+            .catch((error) => {
+                alert(error);
+            })
+    }
+
+
+    render() {
+
+        var url = this.state.url;
+        return (
+            <View style={styles.container}>
+                <WebView
+                    style={styles.content}
+                    source={{
+                        uri: url
+                    }}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    content: {
+        width: deviceWidth,
+        height: deviceHeight,
+        marginTop: MARGIN_TOP
+    }
 });
